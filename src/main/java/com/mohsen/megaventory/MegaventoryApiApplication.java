e9f29a30dbc36c44ed1e9391a8890a6849d12f5a
mohsenre89 <mohsen.rezaei89@yahoo.com>
2024-01-05 12:59:33 +0100

    Initial commit

---
+++ src/main/java/com/mohsen/megaventory/MegaventoryApiApplication.java
@@ -0,0 +1,12 @@
+package com.mohsen.megaventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaventoryApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MegaventoryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohsen/megaventory/MegaventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohsen.megaventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaventoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
